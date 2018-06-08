371ee65ce62a54e2e66eaa786c816a27cd1637eb
Jose Tomas <josetomascodecido@gmail.com>
2018-06-07 20:06:40 -0400

    exercise1

--- 1 metodos/ejercicio1.rb
+++ 1 metodos/ejercicio1.rb
@@ -1,13 +1,5 @@
-# Optimiza el código utilizando tu editor de texto.
-# hint: Debes utilizar 'rubocop'
-# y no obtener errores ni warnings.
-
-def par(x)
-  if x % 2 == 0
-    return true
-  else
-    return false
-  end
+def par(toto)
+  toto.even? ? true : false
 end
 
 puts par(2)
